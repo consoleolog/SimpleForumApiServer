c6510d2d06cfaf22300cca971aaedda3eb33a595
consolelog <leopportunity5@gmail.com>
2024-11-15 17:38:19 +0900

    2024-11-15 :: service sample code 작성

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import {Db} from "mongodb";
+import {Db, MongoClient} from "mongodb";
 import {connectDB} from "../config/database";
 import config from "../config/config";
 import {AccountDto} from "../model/account.dto";
@@ -33,4 +33,35 @@ export const register = async (accountDto: AccountDto) => {
     }
 }
 
+export class AuthService {
 
+    private db: Db | undefined;
+
+    constructor(connection: Promise<MongoClient>) {
+        connection.then((client) => {
+            this.db = client.db(config.db.database);
+        })
+    }
+
+    register = async (accountDto: AccountDto) => {
+        if (this.db === undefined) return ;
+        try {
+
+            const account = await this.db.collection("account").findOne({accountId: accountDto.accountId});
+            if (account) return new ApiResponse(202, responseDatas.ACCEPTED);
+
+            await db.collection("account").insertOne({
+                accountId: accountDto.accountId,
+                accountPw: bcrypt.hash(accountDto.accountPw, 10),
+            });
+
+            return new ApiResponse(201, responseDatas.CREATED);
+        } catch (error){
+            logger.error(`
+        where: services/auth.service.ts
+        message: ${error}
+        `);
+        }
+    }
+
+}
